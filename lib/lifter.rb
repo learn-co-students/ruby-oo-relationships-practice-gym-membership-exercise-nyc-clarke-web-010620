a0aa9653f1c6a420385ce7156b9e178aefe0515b
Granit <ghaxhaj@fordham.edu>
2020-01-14 09:34:11 -0500

    Complete

--- lib/lifter.rb
+++ lib/lifter.rb
@@ -1,8 +1,48 @@
 class Lifter
   attr_reader :name, :lift_total
 
+  @@all = []
+
   def initialize(name, lift_total)
     @name = name
     @lift_total = lift_total
+
+    @@all << self
   end
+
+  def self.all
+    @@all
+  end
+
+  def memberships
+    Membership.all.select do |membership|
+      membership.lifter == self
+    end
+  end
+
+  def gyms 
+    self.memberships.map do |membership|
+      membership.gym
+    end
+  end
+
+  def self.total_lift
+    lift_array = self.all.map {|lifter|lifter.lift_total}
+    lift_array.reduce(0) {|sum,weight| sum += weight}
+  end
+
+  def self.average_lift
+    count = self.all.map {|lifter|lifter.lift_total}.count
+    self.total_lift/count
+  end
+  
+  def gym_price
+    price = self.memberships.map{|gym|gym.cost}
+    price.reduce(0) {|sum,cost| sum += cost}
+  end
+
+  def new_membership(cost,gym)
+    Membership.new(cost,gym,self)
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,7 +3,13 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
-
+l1 = Lifter.new("Granit",400)
+l2 = Lifter.new("Ant",4000)
+g1 = Gym.new("La Fitness")
+g2 = Gym.new("NY Sports")
+m1 = Membership.new(200,g1,l1)
+m2 = Membership.new(100,g2,l1)
+m3 = Membership.new(200,g1,l2)
 binding.pry
 
 puts "Gains!"
